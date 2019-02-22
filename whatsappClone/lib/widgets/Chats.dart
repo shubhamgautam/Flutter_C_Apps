fd7bfb96e0225519b6460a45f2afba7db9d3219d
shubhamgautam <shubhakmgautam@github,com>
2019-02-22 22:44:07 +0530

    updating tile layout

--- whatsappClone/lib/widgets/Chats.dart
+++ whatsappClone/lib/widgets/Chats.dart
@@ -4,15 +4,26 @@ class Chats extends StatelessWidget{
   @override
   Widget build(BuildContext build) {
     return Container(
+      color: Colors.white,
       child: ListView(
         children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
+          Container(
+              child: ListTile(
+              leading: Icon(Icons.map),
+              trailing: Text('Map'),
+            ),
+
           ),
           ListTile(
             leading: Icon(Icons.photo_album),
-            title: Text('Album'),
+            title: DecoratedBox(
+              decoration: BoxDecoration(
+                border: Border(
+                 bottom: BorderSide()
+                ),
+              ),
+              child: Text('Something about it')
+            ),
           ),
           ListTile(
             leading: Icon(Icons.phone),
